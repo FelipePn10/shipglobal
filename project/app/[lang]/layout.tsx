17623053fc68b9d8dad57d8c222db885db35ab0b
Felipe Panosso <felipepanosso21@gmail.com>
2025-03-07 19:35:15 -0300

    remove o ThemeProvider do layout e exclui o componente ThemeProvider

--- project/app/[lang]/layout.tsx
+++ project/app/[lang]/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ThemeProvider } from '@/components/theme-provider'
 import { Navbar } from '@/components/navbar'
 import { Footer } from '@/components/footer'
 import { Toaster } from '@/components/ui/sonner'
@@ -24,17 +23,10 @@ export default function RootLayout({
   return (
     <html lang={lang} suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
           <Navbar currentLanguage={lang} />
           <main>{children}</main>
           <Footer  />
           <Toaster />
-        </ThemeProvider>
       </body>
     </html>
   )
