698a3acaa73af5d976000e6ca79f8718c203a86b
Felipe Panosso <felipepanosso21@gmail.com>
2025-03-13 09:37:42 -0300

    Correção de bugs internos da aplicação

---
+++ project/declarations.d.ts
@@ -0,0 +1,4 @@
+declare module '*.tsx' {
+  const value: any;
+  export default value;
+}
--- project/next.config.js
+++ project/next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.externals.push({
