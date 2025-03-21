bbf2f0f7b813ad722ca393d0fc2a688f90c23476
Felipe Panosso <felipepanosso21@gmail.com>
2025-03-21 17:43:21 -0300

    Adiciona configuração do Webpack, atualiza plugins do PostCSS e ajusta o Tailwind CSS com novas cores e animações

--- project/app/api/placeholder/[...slug]/route.ts
+++ project/app/api/placeholder/[...slug]/route.ts
@@ -1,8 +1,15 @@
 import { NextResponse } from 'next/server';
 
-export async function GET(request: Request, { params }: { params: { slug: string[] } }) {
-  const width = params.slug?.[0] || '150';
-  const height = params.slug?.[1] || '150';
+export async function GET(
+  request: Request,
+  { params }: { params: { slug: string[] } }
+) {
+  // Aguardando o params como um todo antes de acessar sua propriedade
+  const resolvedParams = await params;
+  const slugParams = resolvedParams.slug;
   
+  const width = slugParams?.[0] || '150';
+  const height = slugParams?.[1] || '150';
+ 
   return NextResponse.redirect(`https://via.placeholder.com/${width}x${height}`);
 }
